40d53c28553298066a59ab61e991257717f429c4
shailaja-j <69836494+shailaja-j@users.noreply.github.com>
2022-09-20 15:30:40 +0530

    Delete math.py

--- math.py
+++
@@ -1,7 +0,0 @@
-import math
-
-
-def net_income(revenues: float, expenses: float, decimals=2) -> float:
-    multiplier = 10 ** decimals
-    net_income = revenues - expenses
-    return math.floor(net_income * multiplier + 0.5) / multiplier
