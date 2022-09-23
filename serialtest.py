88704772e05bd89674161f9c1a56711d3008346d
Shailaja Patil <shailaja.p@utthunga.com>
2022-09-23 10:17:29 +0530

    update

--- serialtest.py
+++ serialtest.py
@@ -6,7 +6,7 @@ class serialtest(object):
         ser = serial.Serial ('COM3')    #Open named port
         ser.baudrate = 9600           #Set baud rate to 9600
         #Read ten characters from serial port to data
-        ser.write(b'hello world Utthunga1234')
+        ser.write(b'welcome to utthunga@')
         time.sleep(1)
         data1 = ser.read(11)
         return data1
