bdc3fff935cadf3bcf9423b80a5d4e530bb34fa9
shailaja <shailaja.p@utthunga.com>
2022-10-03 14:24:44 +0530

    update new

--- serialtest.py
+++ serialtest.py
@@ -6,7 +6,7 @@ class serialtest(object):
         ser = serial.Serial ('COM3')    #Open named port
         ser.baudrate = 9600           #Set baud rate to 9600
         #Read ten characters from serial port to data
-        ser.write(b'Hello world')
+        ser.write(b'welcome to galaxy.com')
         time.sleep(1)
         data1 = ser.read(11)
         return data1
