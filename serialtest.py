4c03e2e818e57ccb736f0dd53d1b3a54a68e413f
shailaja-j <69836494+shailaja-j@users.noreply.github.com>
2022-10-06 16:08:15 +0530

    Update serialtest.py

--- serialtest.py
+++ serialtest.py
@@ -6,7 +6,7 @@ class serialtest(object):
         ser = serial.Serial ('COM4')    #Open named port
         ser.baudrate = 9600           #Set baud rate to 9600
         #Read ten characters from serial port to data
-        ser.write(b'welcome to star')
+        ser.write(b'welcome to star12')
         time.sleep(1)
         data1 = ser.read(16)
         return data1
