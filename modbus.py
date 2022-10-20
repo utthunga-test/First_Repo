0094b4ebceacc8c855e19b85ce41bccc6986fd13
shailaja <shailaja.p@utthunga.com>
2022-10-20 13:50:27 +0530

    new

--- modbus.py
+++ modbus.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from pymodbus.client.sync import ModbusSerialClient
 
 client = ModbusSerialClient(
@@ -36,42 +35,4 @@ if client.connect():
 else:
 
     print('cannot connect to the Modbus Server/Slave')
-=======
-from pymodbus.client.sync import ModbusSerialClient
 
-client = ModbusSerialClient(
-
-    method='rtu',
-
-    port='COM2',
-
-    baudrate=9600,
-
-    timeout=0.05,
-
-    parity='N',
-
-    stopbits=1,
-
-    bytesize=8
-
-)
-
-
-
-if client.connect():
-
-    res = client.read_input_registers(address=30000,cout=2,unit=1)
-
-    if not res.isError():
-
-        print(res.registers)
-
-    else:
-
-        print(res)
-
-else:
-
-    print('cannot connect to the Modbus Server/Slave')
->>>>>>> 42003cd2bc27782be5c0043c135cc92797d99a68
