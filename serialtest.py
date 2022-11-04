ba642d22d8b81f5d0e7f68323d26cc97da9a1039
utthunga <shailaja.p@utthunga.com>
2022-11-04 09:55:17 +0530

    update

--- serialtest.py
+++ serialtest.py
@@ -1,12 +1,11 @@
 import serial
 import time
-
 class serialtest(object):
     def test(self):
-        ser = serial.Serial ("/dev/serial")                                                                        ")    #Open named port
-        ser.baudrate = 9600           #Set baud rate to 9600
+        ser = serial.Serial ('/dev/ttyUSB0')                                      
+        ser.baudrate = 9600           
         #Read ten characters from serial port to data
-        ser.write(b'hi ')
+        ser.write(b'hi')
         time.sleep(1)
         data1 = ser.read(11)
         print(data1)
